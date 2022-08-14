ea21ce4a8b032b9bddaacfb3fd9899c45c125b97
NexWonka <willy77889@gmail.com>
2022-08-15 07:32:18 +0800

    done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,20 +25,18 @@
 </section>
 
 <section>
-	<div
-		class="flex flex-row flex-wrap items-center justify-evenly p-10 gap-5 bg-primary"
-	>
+	<div class="flex flex-row flex-wrap items-center justify-evenly p-10 gap-5 bg-primary">
 		<video class="w-56" src="smoking_frog.webm" autoplay muted loop />
 		<div
 			class="flex flex-col gap-5 items-center md:items-start justify-center w-1/2 text-center md:text-left"
 		>
 			<h1 class="text-8xl font-bold text-primary-content">ABOUT $FRGY</h1>
 			<p class="text-primary-content text-justify md:text-left">
-				Frgy (fer-gy) is a community token that consist on rewarding it's holder with BUSD and also by
-				doing community contest to keep the community active!
+				Frgy (fer-gy) is a community token that consist on rewarding it's holder with BUSD and also
+				by doing community contest to keep the community active!
 			</p>
 			<p class="text-primary-content">
-				Froogy plan is to be the next Doge Coin purely by the community!
+				Frgy plan is to be the next Doge Coin purely by the community!
 			</p>
 		</div>
 	</div>
@@ -48,10 +46,75 @@
 	<div class="flex flex-col min-h-screen items-center justify-center p-10 gap-5">
 		<video class="w-56" src="meditating_frog.webm" autoplay muted loop />
 		<h1 class="text-5xl md:text-8xl font-bold text-primary">FRGYNOMICS</h1>
-		<button class="btn w-2/3 md:w-1/3 md:text-xl btn-primary no-animation"> total supply : 1.000.000.000 $FRGY </button>
+		<button class="btn w-2/3 md:w-1/3 md:text-xl btn-primary no-animation">
+			total supply : 1.000.000.000 $FRGY
+		</button>
 		<div class="flex flex-row flex-wrap gap-5 items-center justify-center">
-			<a class="btn btn-primary" href="/">5% Buy Fee</a>
-			<a class="btn btn-primary" href="/">6% Sell Fee</a>
+			<button class="btn btn-primary no-animation"> 5% Buy Fee </button>
+			<button class="btn btn-primary no-animation"> 6% Sell Fee </button>
 		</div>
 	</div>
 </section>
+
+<section>
+	<div
+		class="flex flex-row min-h-screen flex-wrap items-center justify-evenly p-10 gap-5 bg-primary"
+	>
+		<video class="w-64" src="peace_frog.webm" autoplay muted loop />
+		<div class="flex flex-col gap-5 items-center justify-center w-1/2 text-center md:text-left">
+			<h1 class="text-8xl font-bold text-primary-content">FRGYMAP</h1>
+			<div class="flex flex-row flex-wrap md:flex-nowrap text-left gap-5 justify-center">
+				<div class="flex flex-col gap-5 justify-start card bg-base-100 p-10">
+					<p class="text-xl text-primary text-justify md:text-left">Phase 1</p>
+					<ul class="list-disc text-primary leading-loose">
+						<li>Launch on BSC Network</li>
+						<li>Marketing on Telegram</li>
+						<li>Shill Raids on Telegram</li>
+						<li>Token Burn 20%</li>
+						<li>Community Contest</li>
+					</ul>
+				</div>
+				<div class="flex flex-col gap-5 justify-start card bg-base-100 p-10">
+					<p class="text-xl text-primary text-justify md:text-left">Phase 2</p>
+					<ul class="list-disc text-primary leading-loose">
+						<li>Trending on CMS</li>
+						<li>Trending on Dextools</li>
+						<li>Reach 100K Mcap</li>
+						<li>Token Burn 25%</li>
+						<li>Marketing on Twitter</li>
+					</ul>
+				</div>
+				<div class="flex flex-col gap-5 justify-start card bg-base-100 p-10">
+					<p class="text-xl text-primary text-justify md:text-left">Phase 3</p>
+					<ul class="list-disc text-primary leading-loose">
+						<li>Community Contest 2</li>
+						<li>Trending on Twitter</li>
+						<li>Reach 500K Mcap</li>
+						<li>Token Burn 35%</li>
+						<li>More Soon...</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="flex flex-col items-center justify-center p-10 gap-5">
+		<video class="w-56" src="float_frog.webm" autoplay muted loop />
+		<h1 class="text-5xl md:text-8xl font-bold text-primary">FERGYNITY</h1>
+		<p class="text-primary">Join frgy on this ride to take over the world!</p>
+		<a class="btn btn-primary" href="/">$FRGY Community</a>
+	</div>
+</section>
+
+<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+	<div>
+		<p>
+			Made by <a class="link" href="https://github.com/NexWonka" target="_blank">on1force</a> - Want
+			this website? Contact
+			<a class="link" href="https://t.me/tokenvision" target="_blank">@tokenvision</a>
+			/ <a class="link" href="https://t.me/SundayProject" target="_blank">@SundayProject</a>
+		</p>
+	</div>
+</footer>
