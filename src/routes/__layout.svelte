563cf45fe9e6c7a33fea406278c08eb94f4e8f6f
NexWonka <willy77889@gmail.com>
2022-08-15 06:54:02 +0800

    ui 50%

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<meta charset="utf-8" />
+	<link rel="icon" href="%sveltekit.assets%/favicon.png" />
+	<meta name="viewport" content="width=device-width" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/css/splide-core.min.css">
+	<title>FROGGY</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,57 @@
+<script>
+</script>
+
+<section>
+	<div class="flex flex-col min-h-screen items-center justify-center p-10 gap-5">
+		<video class="w-56" src="skating_frog.webm" autoplay muted loop />
+		<h1 class="text-8xl font-bold text-primary">FRGY</h1>
+		<p class="text-primary">The coolest frog on the BSC Ecosystem!</p>
+		<div class="flex flex-row flex-wrap gap-5 items-center justify-center">
+			<a class="btn btn-primary" href="/">Buy $FRGY</a>
+			<a class="btn btn-primary" href="/">$FRGY Chart</a>
+			<a class="btn btn-primary" href="/">$FRGY Community</a>
+		</div>
+		<div class="flex flex-col gap-2 items-center absolute bottom-2 animate-bounce">
+			<svg xmlns="http://www.w3.org/2000/svg" class="w-7" viewBox="0 0 20 20" fill="currentColor">
+				<path
+					fill-rule="evenodd"
+					d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+			<p class="">More Info!</p>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div
+		class="flex flex-row flex-wrap items-center justify-evenly p-10 gap-5 bg-primary"
+	>
+		<video class="w-56" src="smoking_frog.webm" autoplay muted loop />
+		<div
+			class="flex flex-col gap-5 items-center md:items-start justify-center w-1/2 text-center md:text-left"
+		>
+			<h1 class="text-8xl font-bold text-primary-content">ABOUT $FRGY</h1>
+			<p class="text-primary-content text-justify md:text-left">
+				Frgy (fer-gy) is a community token that consist on rewarding it's holder with BUSD and also by
+				doing community contest to keep the community active!
+			</p>
+			<p class="text-primary-content">
+				Froogy plan is to be the next Doge Coin purely by the community!
+			</p>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="flex flex-col min-h-screen items-center justify-center p-10 gap-5">
+		<video class="w-56" src="meditating_frog.webm" autoplay muted loop />
+		<h1 class="text-5xl md:text-8xl font-bold text-primary">FRGYNOMICS</h1>
+		<button class="btn w-2/3 md:w-1/3 md:text-xl btn-primary no-animation"> total supply : 1.000.000.000 $FRGY </button>
+		<div class="flex flex-row flex-wrap gap-5 items-center justify-center">
+			<a class="btn btn-primary" href="/">5% Buy Fee</a>
+			<a class="btn btn-primary" href="/">6% Sell Fee</a>
+		</div>
+	</div>
+</section>
